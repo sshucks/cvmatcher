eaa041dde7f1271b2d566c4a1b4b4ab03a0a54a3
s2410595007 <s2410595007@fhooe.at>
2025-06-04 14:07:55 +0000

    convert docx cvs to pdf before processing

--- src/extracting/cv/process_cvs.py
+++ src/extracting/cv/process_cvs.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 def process_cvs_with_php(input_directory, output_directory, php_script_path):
     os.makedirs(output_directory, exist_ok=True)
     print("Starting CV extraction ...")
-
+    convert_docx_to_pdf(input_directory)
     for filename in tqdm(os.listdir(input_directory)):
         if filename.lower().endswith('.pdf'):
             file_path = os.path.join(input_directory, filename)
@@ -34,3 +34,16 @@ def process_cvs(input_dir, output_dir, php_script):
     process_cvs_with_php(input_dir, output_dir, php_script)
 
 
+def convert_docx_to_pdf(path):
+    """Convert all .docx files in the specified directory to PDF using LibreOffice."""
+    files = os.listdir(path)
+    for filename in files:
+        if filename.endswith(".docx"):
+            input_path = os.path.join(path, filename)
+            subprocess.run([
+                "libreoffice",
+                "--headless",
+                "--convert-to", "pdf",
+                "--outdir", path,
+                input_path
+            ])
