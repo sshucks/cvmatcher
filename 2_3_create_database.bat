5d505714ff76c18fffc97c5341ff422ca7ddcfd9
nikolimaj <S2110458020@students.fh-hagenberg.at>
2025-06-23 10:16:21 +0000

    add database startup script

--- 2_3_create_database.bat
+++ 2_3_create_database.bat
@@ -12,6 +12,6 @@ if %ERRORLEVEL%==0 (
     docker run -it -p 8501:8501 -p 8000:8000 -v "%cd%:/workspaces/cvmatcher" -w /workspaces/cvmatcher -e PYTHONPATH=/workspaces/cvmatcher --name %CONTAINER_NAME% %IMAGE_NAME%
 )
 
-docker exec -it %CONTAINER_NAME% PYTHONPATH=./src python src/extracting/extraction_main.py
+docker exec -it %CONTAINER_NAME% PYTHONPATH=./src python -m caching.database
 
 pause
