069476e9c06c5ab0f388ea405be6d9a155b3eb53
s2410595007 <s2410595007@fhooe.at>
2025-12-03 12:17:25 +0000

    Added code docu

--- matching/esco/matching.py
+++ matching/esco/matching.py
@@ -4,7 +4,12 @@ from sentence_transformers import SentenceTransformer
 import pandas as pd
 
 class EscoMatchingStep(MatchingStep):
+    """ Abstract class for matching with ESCO """
     def __init__(self, args):
+        """ 
+        :param args: arguments for the class (mandatory: esco_language)
+        :type args: Dict
+        """
         super().__init__()
         
         self.language = args.get("esco_language")
@@ -57,7 +62,12 @@ class EscoMatchingStep(MatchingStep):
 
 
 class EducationEscoMatchingStep(EscoMatchingStep):
+    """ Class for matching Education via ESCO jobs """
     def __init__(self, args):
+        """ 
+        :param args: arguments for the class (mandatory: esco_language)
+        :type args: Dict
+        """
         super().__init__(args)
 
         self.occupations = e.read_occupations(self.language)
@@ -66,6 +76,18 @@ class EducationEscoMatchingStep(EscoMatchingStep):
         self.occupation_preferredLabels = e.read_occupation_preferredLabels(self.language)
 
     def run(self, education_cv: EducationData, education_req: EducationData, args) -> float:
+        """ Do education matching of cv and requirement
+
+        :param education_cv: EducationData of CV
+        :type education_cv: EducationData
+        :param education_req: EducationData of Requirements
+        :type education_req: EducationData
+        :param args: parameters for the matching (mandatory: esco_job_decay)
+        :type args: Dict
+
+        :return: Value 0-1, how well are the requirements covered in the cv
+        :rtype: float
+        """
         decay = args.get("esco_job_decay")
 
         cv_labels = [ed["field_of_study"] for ed in education_cv]
@@ -76,15 +98,28 @@ class EducationEscoMatchingStep(EscoMatchingStep):
 
         score, matches = super().hierarchical_matching(req_uris, cv_uris, self.lookup, decay)
 
-        #print(e.get_job_labels(matches, self.occupation_preferredLabels))
+        #print(self.label(matches))
         return score
     
-    def label(self, uris):
+    def label(self, uris:set[str]):
+        """ Get ESCO preferred labels for given job uris
+        
+        :param uris: job ESCO uris
+        :type uris: set[str]
+
+        :return: Preferred labels for all given job uris
+        :rtype: set[str]
+        """
         return e.get_job_labels(uris, self.occupation_preferredLabels)
 
 
 class ExperienceEscoMatchingStep(EscoMatchingStep):
+    """ Class for matching Experience with ESCO """
     def __init__(self, args):
+        """ 
+        :param args: arguments for the class (mandatory: esco_language)
+        :type args: Dict
+        """
         super().__init__(args)
 
         self.occupations = e.read_occupations(self.language)
@@ -93,6 +128,18 @@ class ExperienceEscoMatchingStep(EscoMatchingStep):
         self.occupation_preferredLabels = e.read_occupation_preferredLabels(self.language)
 
     def run(self, experience_cv: ProfessionalExperienceData, experience_req: ProfessionalExperienceData, args) -> float:
+        """ Do experience matching of cv and requirement
+
+        :param experience_cv: ProfessionalExperienceData of CV
+        :type experience_cv: ProfessionalExperienceData
+        :param experience_req: ProfessionalExperienceData of Requirements
+        :type experience_req: ProfessionalExperienceData
+        :param args: parameters for the matching (mandatory: esco_job_decay)
+        :type args: Dict
+
+        :return: Value 0-1, how well are the requirements covered in the cv
+        :rtype: float
+        """
         decay = args.get("esco_job_decay")
 
         cv_labels = [ex["job_title"] for ex in experience_cv]
@@ -103,15 +150,28 @@ class ExperienceEscoMatchingStep(EscoMatchingStep):
 
         score, matches = super().hierarchical_matching(req_uris, cv_uris, self.lookup, decay)
 
-        #print(e.get_job_labels(matches, self.occupation_preferredLabels))
+        #print(self.label(matches))
         return score
     
-    def label(self, uris):
+    def label(self, uris:set[str]):
+        """ Get ESCO preferred labels for given job uris
+        
+        :param uris: job ESCO uris
+        :type uris: set[str]
+
+        :return: Preferred labels for all given job uris
+        :rtype: set[str]
+        """
         return e.get_job_labels(uris, self.occupation_preferredLabels)
     
 
 class SkillEscoMatchingStep(EscoMatchingStep):
+    """ Class for matching Skills with ESCO """
     def __init__(self, args):
+        """ 
+        :param args: arguments for the class (mandatory: esco_language)
+        :type args: Dict
+        """
         super().__init__(args)
 
         self.skills = e.read_skills(self.language)
@@ -120,6 +180,18 @@ class SkillEscoMatchingStep(EscoMatchingStep):
         self.skill_preferredLabels = e.read_skill_preferredLabels(self.language)
 
     def run(self, skills_cv: list[str], skills_req: list[str], args) -> float:
+        """ Do skill matching of cv and requirement
+
+        :param skills_cv: skills of CV
+        :type skills_cv: list[str]
+        :param skills_req: skills of Requirements
+        :type skills_req: list[str]
+        :param args: parameters for the matching (mandatory: esco_skill_decay)
+        :type args: Dict
+
+        :return: Value 0-1, how well are the requirements covered in the cv
+        :rtype: float
+        """
         decay = args.get("esco_skill_decay")
 
         cv_uris = e.get_skill_uris(set(skills_cv), self.skill_preferredLabels)
@@ -127,8 +199,16 @@ class SkillEscoMatchingStep(EscoMatchingStep):
 
         score, matches = super().hierarchical_matching(req_uris, cv_uris, self.lookup, decay)
 
-        #print(e.get_skill_labels(matches,self.skill_preferredLabels))
+        #print(self.label(matches))
         return score
     
-    def label(self, uris):
+    def label(self, uris: set[str]):
+        """ Get ESCO preferred labels for given skill uris
+        
+        :param uris: skill ESCO uris
+        :type uris: set[str]
+
+        :return: Preferred labels for all given skill uris
+        :rtype: set[str]
+        """
         return e.get_skill_labels(uris, self.skill_preferredLabels)
--- parse_cv/esco/translate_cv.py
+++ parse_cv/esco/translate_cv.py
@@ -5,7 +5,12 @@ from sentence_transformers import SentenceTransformer
 
 
 class ESCOTranslateCV():
+    """ Class to translate requirements to standardized ESCO terms """
     def __init__(self, args):
+        """ 
+        :param args: arguments for the class (mandatory: esco_language)
+        :type args: Dict
+        """
         self.model = SentenceTransformer("TechWolf/JobBERT-v3")
         self.language = args.get("esco_language")
 
@@ -15,30 +20,57 @@ class ESCOTranslateCV():
         self.skill_preferredLabels = e.read_skill_preferredLabels(self.language)
         self.occupation_preferredLabels = e.read_occupation_preferredLabels(self.language)
 
-
-    """ Class to translate requirements to standardized ESCO terms """
+    
     def run(self, cv: CVData, args) -> CVData:
-        job_threshold = args.get("esco_job_threshold")
-        education_threshold = args.get("esco_education_threshold")
-        skill_threshold = args.get("esco_skill_threshold")
+        """ translate full CV
+        
+        :param cv: CV to be translated
+        :type cv: CVData
+        :param args: translation parameters (mandatory: esco_job_threshold, esco_education_threshold, esco_skill_threshold)
+        :type args: Dict
+        
+        :return: translated CV
+        :rtype: CVData
+        """
+        self.job_threshold = args.get("esco_job_threshold")
+        self.education_threshold = args.get("esco_education_threshold")
+        self.skill_threshold = args.get("esco_skill_threshold")
 
 
-        cv["hard_skills"] = self.translate_skills(cv["hard_skills"], skill_threshold, 3)
-        cv["soft_skills"] = self.translate_skills(cv["soft_skills"], skill_threshold, 3)
-        cv["professional_experience"] = self.translate_prof_experience(cv["professional_experience"], job_threshold)
-        cv["education"] = self.translate_education_via_jobs(cv["education"], education_threshold)
+        cv["hard_skills"] = self.translate_skills(cv["hard_skills"], 3)
+        cv["soft_skills"] = self.translate_skills(cv["soft_skills"], 3)
+        cv["professional_experience"] = self.translate_prof_experience(cv["professional_experience"])
+        cv["education"] = self.translate_education_via_jobs(cv["education"])
 
         return cv
 
 
-    def translate_skills(self, skills:list[str], label_threshold:float, n:int = 1):
-        esco_uris = e.ESCO_labelling(skills, self.esco_skills, self.model, label_threshold, n, f"embeddings_skills_{self.language}.npy", self.language)
+    def translate_skills(self, skills:list[str], n:int = 1):
+        """ translate skills to standardized ESCO terms
+
+        :param skills: skills to be translated
+        :type skills: list[str]
+        :param n: max number of ESCO terms to be chosen per skill
+        :type n: int
+
+        :return: translated skills
+        :rtype: list[str] 
+        """
+        esco_uris = e.ESCO_labelling(skills, self.esco_skills, self.model, self.skill_threshold, n, f"embeddings_skills_{self.language}.npy", self.language)
         esco_labels = e.get_skill_labels(esco_uris, self.skill_preferredLabels)
         return list(esco_labels)
     
 
-    def translate_job_title(self, job_title:str, label_threshold:float, n:int = 1):
-        esco_uris = e.ESCO_labelling([job_title], self.esco_occupation, self.model, label_threshold, n, f"embeddings_occupations_{self.language}.npy", self.language)
+    def translate_job_title(self, job_title:str):
+        """ translate job title to standardized ESCO term
+
+        :param job_title: job title to be translated
+        :type job_title: str
+
+        :return: translated job title
+        :rtype: str
+        """
+        esco_uris = e.ESCO_labelling([job_title], self.esco_occupation, self.model, self.job_threshold, 1, f"embeddings_occupations_{self.language}.npy", self.language)
         esco_labels = e.get_job_labels(esco_uris, self.occupation_preferredLabels)
 
         if len(esco_labels) < 1:
@@ -47,24 +79,40 @@ class ESCOTranslateCV():
         return list(esco_labels)[0]
     
 
-    def translate_prof_experience(self, prof_experience: list[ProfessionalExperienceData], label_threshold:float):
+    def translate_prof_experience(self, prof_experience: list[ProfessionalExperienceData]):
+        """ translate experience to standardized ESCO terms
+
+        :param prof_experience: experience to be translated
+        :type prof_experience: list[ProfessionalExperienceData]
+
+        :return: translated experience
+        :rtype: list[ProfessionalExperienceData] 
+        """
         translated = []
 
         for prof_ex in prof_experience:
             translated_prof = copy.deepcopy(prof_ex)
-            translated_prof["job_title"] = self.translate_job_title(prof_ex["job_title"], label_threshold)
-            translated_prof["responsibilities"] = list(self.translate_skills(prof_ex["responsibilities"], label_threshold, 3))
+            translated_prof["job_title"] = self.translate_job_title(prof_ex["job_title"])
+            translated_prof["responsibilities"] = list(self.translate_skills(prof_ex["responsibilities"], 3))
 
             translated.append(translated_prof)
 
         return translated
 
-    def translate_education_via_jobs(self, education: list[EducationData], label_threshold:float):
+    def translate_education_via_jobs(self, education: list[EducationData]):
+        """ translate education to standardized ESCO terms
+
+        :param education: education to be translated
+        :type education: list[EducationData]
+
+        :return: translated education
+        :rtype: list[EducationData] 
+        """
         translated = []
 
         for ed in education:
             ed_copy = copy.deepcopy(ed)
-            ed_copy["field_of_study"] = self.translate_job_title(ed["field_of_study"], label_threshold)
+            ed_copy["field_of_study"] = self.translate_job_title(ed["field_of_study"])
 
             translated.append(ed_copy)
 
@@ -73,35 +121,68 @@ class ESCOTranslateCV():
 
 
 class ESCOEnrichCV(ESCOTranslateCV):
+    """ Class to translate requirements to standardized ESCO terms and enrich with ESCO relationships """
     def __init__(self, args):
+        """ 
+        :param args: arguments for the class (mandatory: esco_language)
+        :type args: Dict
+        """
+
         super().__init__(args)
         self.skill_lookup = e.read_skill_hierarchy(self.language)
         self.skill_occ_relation = e.read_skill_occ_relation(self.language)
 
-    """ Class to translate requirements to standardized ESCO terms and enrich with ESCO relationships """
     def run(self, cv: CVData, args) -> CVData:
-        job_threshold = args.get("esco_job_threshold")
-        education_threshold = args.get("esco_education_threshold")
-        skill_threshold = args.get("esco_skill_threshold")
+        """ translate and enrich full CV
+        
+        :param cv: CV to be translated
+        :type cv: CVData
+        :param args: translation parameters (mandatory: esco_job_threshold, esco_education_threshold, esco_skill_threshold)
+        :type args: Dict
+        
+        :return: translated and enriched CV
+        :rtype: CVData
+        """
+        self.job_threshold = args.get("esco_job_threshold")
+        self.education_threshold = args.get("esco_education_threshold")
+        self.skill_threshold = args.get("esco_skill_threshold")
 
-        cv["professional_experience"] = super().translate_prof_experience(cv["professional_experience"], job_threshold)
-        cv["education"] = super().translate_education_via_jobs(cv["education"], education_threshold)
-        cv["hard_skills"] = self.translate_skills_with_hierarchy(cv["hard_skills"], skill_threshold, 3)
-        cv["hard_skills"] += self.enrich_hard_skills_with_exp(cv["professional_experience"], skill_threshold)
+        cv["professional_experience"] = super().translate_prof_experience(cv["professional_experience"])
+        cv["education"] = super().translate_education_via_jobs(cv["education"])
+        cv["hard_skills"] = self.translate_skills_with_hierarchy(cv["hard_skills"], 3)
+        cv["hard_skills"] += self.enrich_hard_skills_with_exp(cv["professional_experience"])
         cv["hard_skills"] = list(set(cv["hard_skills"]))
-        cv["soft_skills"] = self.translate_skills_with_hierarchy(cv["soft_skills"], skill_threshold, 3)
+        cv["soft_skills"] = self.translate_skills_with_hierarchy(cv["soft_skills"], 3)
 
         return cv
 
     
-    def translate_skills_with_hierarchy(self, skills:list[str], label_threshold:float, n:int = 1):
-        esco_uris = e.ESCO_labelling(skills, self.esco_skills, self.model, label_threshold, n, f"embeddings_skills_{self.language}.npy", self.language)
+    def translate_skills_with_hierarchy(self, skills:list[str], n:int = 1):
+        """ translate skills to standardized ESCO terms and enrich with broader hierarchical terms
+
+        :param skills: skills to be translated
+        :type skills: list[str]
+        :param n: max number of ESCO terms to be chosen per skill
+        :type n: int
+
+        :return: translated and enriched skills
+        :rtype: list[str] 
+        """
+        esco_uris = e.ESCO_labelling(skills, self.esco_skills, self.model, self.skill_threshold, n, f"embeddings_skills_{self.language}.npy", self.language)
         esco_enriched = e.enrich_ESCO(esco_uris, self.skill_lookup)
         esco_labels = e.get_skill_labels(esco_enriched, self.skill_preferredLabels)
         return list(esco_labels)
     
 
-    def enrich_hard_skills_with_exp(self, prof_experience:list[ProfessionalExperienceData], label_threshold:float):
+    def enrich_hard_skills_with_exp(self, prof_experience:list[ProfessionalExperienceData]):
+        """ extract skills from professional experience and enrich with broader hierarchical terms
+
+        :param prof_experience: professional experience of which skills should be extracted
+        :type prof_experience: list[ProfessionalExperienceData]
+
+        :return: extracted and enriched skills
+        :rtype: list[str] 
+        """
         profs = []
         resp = []
 
@@ -109,9 +190,9 @@ class ESCOEnrichCV(ESCOTranslateCV):
             profs.append(prof_exp["job_title"])
             resp += prof_exp["responsibilities"]
 
-        job_uris =  e.ESCO_labelling(profs, self.esco_occupation, self.model, label_threshold, 1, f"embeddings_occupations_{self.language}.npy", self.language)
+        job_uris =  e.ESCO_labelling(profs, self.esco_occupation, self.model, self.job_threshold, 1, f"embeddings_occupations_{self.language}.npy", self.language)
         skill_uris = e.get_skills_for_occ(job_uris, self.skill_occ_relation)
-        skill_uris = skill_uris | e.ESCO_labelling(resp, self.esco_skills, self.model, label_threshold, 3, f"embeddings_skills_{self.language}.npy", self.language)
+        skill_uris = skill_uris | e.ESCO_labelling(resp, self.esco_skills, self.model, self.skill_threshold, 3, f"embeddings_skills_{self.language}.npy", self.language)
 
         skills_enriched = e.enrich_ESCO(skill_uris, self.skill_lookup)
         skills = e.get_skill_labels(skills_enriched, self.skill_preferredLabels)
